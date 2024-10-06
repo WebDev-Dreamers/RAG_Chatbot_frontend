ffdd9cdbdf555b8acdde8b7e48ddb5c03069c537
hajin0324 <hajinn24@gmail.com>
2024-10-06 14:19:49 +0900

    style: eslint, prettier 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.js'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 0,
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
-}
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+};
